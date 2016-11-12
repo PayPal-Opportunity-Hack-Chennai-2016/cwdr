d9e7025b503acbee9c829b24891a25539f4b69e6
Sandeep Gupta <sandeepgupta2k4@yahoo.co.in>
2016-11-13 00:19:33 +0530

    commit

--- controllers/login.js
+++ controllers/login.js
@@ -6,16 +6,16 @@ var Q = require('q');
 
 exports = module.exports = {
 	login: function login(req, callback) {
-		if (!req.body.password || !req.body.userName) {
+		if (!req.body.password || !req.body.email) {
 			callback({error: "User Name or password is not correct"}, null);
 		}
-		var loginPromise = Q.ninvoke(db,'searchUser',req.body.userName);
+		var loginPromise = Q.ninvoke(db,'searchUser',req.body.email);
 		loginPromise.then(function(response) {
 			if (response.password && response.password === req.body.password) {
-				var contentPromise = Q.ninvoke(db,'getContent',req);
-				contentPromise.then(function(response) {
+				var contentPromise = Q.ninvoke(content,'getContentForUser',response);
+				contentPromise.then(function(result) {
 					console.log('got content');
-					callback(null, response);
+					callback(null, result);
 
 				}).catch(function(err) {
 					callback({error: "Not able to get the content"}, null);
